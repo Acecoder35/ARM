92fcd6ed01c55eb746e8875f6e5017a821c6533d
Acecoder35 <codeid35@gmail.com>
2021-08-14 21:03:26 +0530

    Add files via upload

---
+++ Assignment1/third.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int arr[20];
+	for(int i=0; i<20; i++){
+	    scanf("%d", &arr[i]);
+	}
+	
+	int count = 0;
+	
+	for(int i=0; i<20; i++){
+	    if(arr[i]%2 == 0){
+	        count++;
+	    }
+	}
+	
+	printf("%d", count);
+	return 0;
+}
